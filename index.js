c96d05fa9299087b83e8e986e3ac1971b1780b15
Vivekhopper <bhojanamvivek1111@gmail.com>
2024-07-10 17:55:43 +0530

    second commit

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import Connection from "./config/DbConnection.js";
 import cors from "cors";
-import  Router  from "./routes/routes.js";
+import Router from "./routes/routes.js";
+
+dotenv.config({ path: "./config/.env" });
 
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin:["https://contact-front-end-fawn.vercel.app"],
-  methods:["GET","PUT","POST","DELETE"],
-  credentials:true
+  origin: ["https://contact-front-end-fawn.vercel.app/"],
+  methods: ["GET", "PUT", "POST", "DELETE"],
+  credentials: true,
 }));
 app.use("/contact", Router);
-dotenv.config({ path: "./config/.env" });
 
-// Establish the database connection
 Connection()
   .then(() => {
-    // Start the server only after the DB connection is established
     app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     });
