6cee6d9bbecf77608547befaae6208bd6e655899
Vivekhopper <bhojanamvivek1111@gmail.com>
2024-07-10 18:17:57 +0530

    forth commit

--- index.js
+++ index.js
@@ -8,11 +8,35 @@ dotenv.config({ path: "./config/.env" });
 
 const app = express();
 app.use(express.json());
+
+const allowedOrigins = [
+  "https://contact-front-end-fawn.vercel.app",
+];
+
 app.use(cors({
-  origin: "https://contact-front-end-fawn.vercel.app",
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   methods: ["GET", "PUT", "POST", "DELETE"],
   credentials: true,
 }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://contact-front-end-fawn.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use("/contact", Router);
 
